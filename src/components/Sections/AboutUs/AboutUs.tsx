0e805c37b317b290076569d4bea2f4bb614bdd5d
Mhamad Ghoche <bachirmohamad96@gmail.com>
2025-06-28 13:00:27 +0300

    fixing eslint error

--- src/components/Sections/AboutUs/AboutUs.tsx
+++ src/components/Sections/AboutUs/AboutUs.tsx
@@ -4,7 +4,7 @@ export default function AboutUs() {
     <div className="bg-white mt-20 h-100 flex flex-col justify-center items-center p-10  md:p-40">
       <h1 className="text-3xl font-bold text-center color-primary">About Us</h1>
       <p className="text-lg text-center color-primary">
-        We're a small business creating handmade soy candles and artisanal
+        We&apos;re a small business creating handmade soy candles and artisanal
         macramé pieces — all designed to bring cozy, earthy vibes into your
         home. Every piece is made with love, sustainability, and attention to
         detail.
