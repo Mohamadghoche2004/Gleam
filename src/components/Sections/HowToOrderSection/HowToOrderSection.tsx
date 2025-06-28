bcef4ce39d5c83a83a36976f0215dcb720a95cff
Mhamad Ghoche <bachirmohamad96@gmail.com>
2025-06-28 15:44:37 +0300

    fixing designs of how to order section

--- src/components/Sections/HowToOrderSection/HowToOrderSection.tsx
+++ src/components/Sections/HowToOrderSection/HowToOrderSection.tsx
@@ -1,17 +1,12 @@
-"use client";
-import { Heart, Money, ShoppingBag } from "phosphor-react";
-
 export default function HowToOrderSection() {
   return (
     <div className=" flex flex-col items-center justify-center bg-white p-10 md:p-30 gap-10">
-      <h1 className="text-4xl font-bold text-center mb-5 color-primary">
+      <h1 className="text-3xl md:text-4xl font-bold text-center mb-5 color-primary">
         How to Order ?
       </h1>
       <div className="flex flex-col items-center justify-center">
-        <span className="text-2xl font-bold text-center mb-2 color-primary flex items-center justify-center gap-2">
-          <Heart size={20} className="text-primary" />
+        <span className="text-xl md:text-2xl font-bold text-center mb-2 color-primary flex items-center justify-center gap-2">
           Step 1 - Browse Your Favorites{" "}
-          <Heart size={20} className="text-primary" />
         </span>
         <p className="text-lg text-center color-primary">
           Explore our handcrafted candles and macrame creations. Pick what you
@@ -19,10 +14,8 @@ export default function HowToOrderSection() {
         </p>
       </div>
       <div className="flex flex-col items-center justify-center">
-        <span className="text-2xl font-bold text-center mb-2 color-primary flex items-center justify-center gap-2">
-          <ShoppingBag size={20} className="text-primary" />
+        <span className="text-xl md:text-2xl font-bold text-center mb-2 color-primary flex items-center justify-center gap-2">
           Step 2 - Place Your Order{" "}
-          <ShoppingBag size={20} className="text-primary" />
         </span>
         <p className="text-lg text-center color-primary">
           Send us a message on WhatsApp or fill out our order form. We’ll
@@ -30,9 +23,8 @@ export default function HowToOrderSection() {
         </p>
       </div>
       <div className="flex flex-col items-center justify-center">
-        <span className="text-2xl font-bold text-center mb-2 color-primary flex items-center justify-center gap-2">
-          <Money size={20} className="text-primary" /> Step 3 - Pay on Delivery{" "}
-          <Money size={20} className="text-primary" />
+        <span className="text-xl md:text-2xl font-bold text-center mb-2 color-primary flex items-center justify-center gap-2">
+          Step 3 - Pay on Delivery{" "}
         </span>
         <p className="text-lg text-center color-primary">
           We deliver straight to your door. No online payments — just pay in
